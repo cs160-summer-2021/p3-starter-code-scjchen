91455b137c2a987850b0a4e939498f911b0dbfd6
Sophie Chen <sophie.cj.chen@berkeley.edu>
2021-07-13 01:40:56 -0700

    art community+drawing

--- coloring/urls.py
+++ coloring/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('demo', views.index, name='demo'),
-    path('new_interaction', views.index, name='new_interaction')
+    path('index', views.index, name='index'),
+    path('demo', views.demo, name='demo'),
+    path('artwork_page_3', views.artwork_page_3, name='artwork_page_3'),
+    path('artwork_page_2', views.artwork_page_2, name='artwork_page_2'),
+    path('artwork_page_1', views.artwork_page_1, name='artwork_page_1'),
+    path('art_community_main_page', views.art_community_main_page, name='art_community_main_page'),
+    path('new_interaction', views.new_interaction, name='new_interaction'),
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+
+def demo(request):
+    return render(request, 'coloring/demo.html')
+
+def artwork_page_3(request):
+    return render(request, 'coloring/artwork_page_3.html')
+
+def artwork_page_2(request):
+    return render(request, 'coloring/artwork_page_2.html')
+
+def artwork_page_1(request):
+    return render(request, 'coloring/artwork_page_1.html')
+
+def art_community_main_page(request):
+    return render(request, 'coloring/art_community_main_page.html')
+
+def new_interaction(request):
+    return render(request, 'coloring/new_interaction.html')
